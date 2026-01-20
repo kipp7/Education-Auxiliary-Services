22c1ad02432a85f0963765638f582b2f906be802
kipp7 <13377035712@163.com>
2026-01-20 21:23:57 +0800

    01-miniapp: fix simulator startup (wxml bindtap expression)

--- modules/01-miniapp/miniprogram/pages/index/index.js
+++ modules/01-miniapp/miniprogram/pages/index/index.js
@@ -50,9 +50,8 @@ Page({
     wx.switchTab({ url: '/pages/my/index' })
   },
 
-  goMyOrLogin() {
-    const userInfo = this.data.userInfo || {}
-    if (userInfo.avatarUrl) {
+  onAvatarTap() {
+    if (this.data.userInfo && this.data.userInfo.avatarUrl) {
       this.goMy()
       return
     }
@@ -138,4 +137,3 @@ Page({
     wx.navigateTo({ url: `/pages/learn/index?cateid=${cateid}&menu=${encodeURIComponent(name)}` })
   }
 })
-
